9ed45439d76a6b6e4c7bc09861e36e2d521482d9
Nick Galang <nicholasgalang@gmail.com>
2026-02-14 14:09:16 -1000

    Fix: Remove deprecated appDir experimental flag from next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['videos.pexels.com'],
+    domains: ['videos.pexels.com', 'images.pexels.com'],
   },
 }
 

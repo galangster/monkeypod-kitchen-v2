58f8605a68aaad2caa54503060db08d426fa8555
Nick Galang <nicholasgalang@gmail.com>
2026-02-16 19:59:01 -1000

    fix: Add Unsplash image domains and unoptimized config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['videos.pexels.com', 'images.pexels.com'],
+    domains: [
+      'videos.pexels.com',
+      'images.pexels.com',
+      'images.unsplash.com',
+      'plus.unsplash.com'
+    ],
+    unoptimized: true,
   },
 }
 
